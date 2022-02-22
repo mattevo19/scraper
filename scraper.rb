7daee3b8812625e7b72ec0a8867c967e71f7a84b
matt <mattevo10@gmail.com>
2022-02-22 20:24:29 +0100

    now gets url

--- scraper.rb
+++ scraper.rb
@@ -4,13 +4,13 @@ require 'open-uri'
 
 def scraper
   url = "https://www.skysports.com/football"
-  unparsed_page = URI.open(url)
-  parsed_page = Nokogiri::HTML(unparsed_page)
-  articles = parsed_page.css('h4.news-list__headline')
   posts = []
+ 
+  articles = Nokogiri::HTML(URI.open(url)).css('h4.news-list__headline')
   articles.each do |article|
     post = {
-      title: article.css('a.news-list__headline-link').text.gsub(/^\s*|\s*$/, "")
+      title: article.css('a.news-list__headline-link').text.gsub(/^\s*|\s*$/, ""),
+      url: articles.css('a')[0].attributes['href'].value
     }
     posts << post
   end
